7142f9465e5d2d8ae42a9fd9e9d2bb88af1ad728
MaciekTalaska <maciek.talaska@gmail.com>
2015-06-27 11:40:40 +0200

    checking for 404 page

--- tests.js
+++ tests.js
@@ -1,8 +1,15 @@
 module.exports = {
-  'Password recovery page should allow user to ': function(test) {
+  '	0000290 - Password recovery page should allow user to ': function(test) {
     test
       .open('http://demo.testarena.pl/odzyskaj_haslo')
       .assert.attr('.remember_check > a', 'href', 'http://demo.testarena.pl/zaloguj')
+      .done();
+  },
+
+  '	0000272 - 404 page should exist ': function(test) {
+    test
+      .open('http://demo.testarena.pl/zalogujABCD')
+      .assert.exists('h1.pagenotfound_title', '404 page should exist')
       .done();
   }
 };
